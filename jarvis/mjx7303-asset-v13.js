505bb8b79cd552d51e9ce434600d2962e017d9b9
gymwhaleysspot-dot <gymwhaleysspot@gmail.com>
2026-08-07 14:41:48 -0400

    Route Racing asset bridge to V19 solid body

--- jarvis/mjx7303-asset-v13.js
+++ jarvis/mjx7303-asset-v13.js
@@ -1,2 +1,2 @@
-// MJX7303_ASSET_V13 compatibility bridge -> closed-body MJX7303_ASSET_V18
-export {AI7303Modeler,buildGarageEnvironment,semanticRegions,ASSET_VERSION} from './mjx7303-asset-v18.js?v=20260807body18';
+// MJX7303_ASSET_V13 compatibility bridge -> solid-body MJX7303_ASSET_V19
+export {AI7303Modeler,buildGarageEnvironment,semanticRegions,ASSET_VERSION} from './mjx7303-asset-v19.js?v=20260807body19';
